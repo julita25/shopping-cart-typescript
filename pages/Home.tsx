9d6ab5467b33c69990da487c93aff0507159771b
juli <julitati25@gmail.com>
2022-12-04 11:53:56 +0300

    add pages

---
+++ pages/Home.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <div style={{ background: "#C0EEE4" }}>
+        <img
+          src="/imgs/grocery-bg.jpg"
+          style={{ height: "100%", width: "100%", objectFit: "cover" }}
+        />
+        <div
+          style={{
+            top: "10px",
+            left: "10px",
+            fontSize: "25px"
+          }}>
+          Start your online order now!</div>
+      </div>
+    </>
+  )
+}
