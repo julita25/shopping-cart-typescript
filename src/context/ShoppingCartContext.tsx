c0bb05357fb97a0884e271623c30573706171a39
juli <julitati25@gmail.com>
2022-12-05 12:58:32 +0300

    fix in id number

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -41,13 +41,14 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
 
   const openCart = () => setIsOpen(true);
   const closeCart = () => setIsOpen(false);
+
   function getItemQuantity(id: number) {
     return cartItems.find(item => item.id === id)?.quantity || 0
   };
 
   function increaseCartQuantity(id: number) {
     setCartItems(currItems => {
-      if (currItems.find(item => item.id === id) == null) {
+      if (!currItems.find(item => item.id === id)) {
         return [...currItems, { id, quantity: 1 }]
       } else {
         return currItems.map(item => {
